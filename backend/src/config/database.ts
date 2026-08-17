7a2319705ec19b2910984d4e654637320034e16c
Sid <sidalbertok@gmail.com>
2026-08-17 18:32:13 -0300

    fix: conserta o database.ts

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -13,8 +13,8 @@ export const AppDataSource = new DataSource({
     username: DB_USER,          // usuário do banco
     password: DB_PASSWORD,          // senha do banco
     database: DB_DATABASE,   // nome do banco de dados
-    synchronize: true, // isso cria/atualiza as tabelas automaticamente — CUIDADO: em produção coloca false pra não perder dados!
-    logging: true,                        // mostra no terminal todas as queries SQL que rodam
+    synchronize: false, // isso cria/atualiza as tabelas automaticamente — CUIDADO: em produção coloca false pra não perder dados!
+    logging: false,                        // mostra no terminal todas as queries SQL que rodam
     entities: [Usuario,Remedio],                  // lista de "tabelas" que o sistema vai gerenciar
 });
 
