72c651278bcf88f52c888c600b5b8cd25a77e207
Sid <sidalbertok@gmail.com>
2026-08-17 20:38:19 -0300

    Fix: conserta configuracoes

--- medtime/assets/Pages/PaginaConfiguracoes.tsx
+++ medtime/assets/Pages/PaginaConfiguracoes.tsx
@@ -21,7 +21,7 @@ import logo from '../Pages/logo.png';
 import MenuLateral from '../Components/MenuLateral';
 import { useAuth } from '../Contexts/AuthContext';
 
-const API_URL = 'http://172.20.86.189:3000';
+const API_URL = 'https://backend-or-main-production-a372.up.railway.app' // ✅
 
 const PaginaConfiguracoes = ({ navigation }: any) => {
   const { user, token, logout, updateUser } = useAuth();
